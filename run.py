df17dfdc8fff10679221dde4f7c2a28e33f01333
Esmaeil Chitgar <esmaeil.chitgar@Snapp-Box.com>
2026-09-01 15:26:27 +0330

    EiFormer3

--- run.py
+++ run.py
@@ -16,8 +16,8 @@ if __name__ == '__main__':
     # basic config
     parser.add_argument('--is_training', type=int, required=True, default=1, help='status')
     parser.add_argument('--model_id', type=str, required=True, default='test', help='model id')
-    parser.add_argument('--model', type=str, required=True, default='iTransformer',
-                        help='model name, options: [iTransformer, iInformer, iReformer, iFlowformer, iFlashformer]')
+    parser.add_argument('--model', type=str, required=True, default='iTransformer_EiFormer',
+                        help='model name, options: [iTransformer, iInformer, iReformer, iFlowformer, iFlashformer, iTransformer_EiFormer]')
 
     # data loader
     parser.add_argument('--data', type=str, required=True, default='custom', help='dataset type')
--- scripts/multivariate_forecasting/ECL/ixTransformer.bat
+++ scripts/multivariate_forecasting/ECL/ixTransformer.bat
@@ -1,6 +1,6 @@
 @echo off
 set CUDA_VISIBLE_DEVICES=0
-set model_name=EiFormer
+set model_name=iTransformer_EiFormer
 
 python -u run.py ^
   --is_training 1 ^
